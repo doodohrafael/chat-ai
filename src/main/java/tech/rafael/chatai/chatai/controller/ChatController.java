2a04aff2a619bd8f21c76b992d0e62b989fc2f07
Douglas Rafael <doodohrafael@gmail.com>
2026-03-21 12:23:09 -0300

    - Permissao CORS apontando para o DNS producao do frontent.

--- src/main/java/tech/rafael/chatai/chatai/controller/ChatController.java
+++ src/main/java/tech/rafael/chatai/chatai/controller/ChatController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173", "http://chataifront.com.br"})
+@CrossOrigin(origins = {"http://localhost:5173", "https://chat-ai-frontend-661628920758.southamerica-east1.run.app"})
 public class ChatController {
 
     private final ChatClient chatClient;
